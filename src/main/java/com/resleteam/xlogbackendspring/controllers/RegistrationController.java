0ba332667883afff04b59636c514a52cfafe54e7
Kirill <kirill.zubyk@gmail.com>
2024-05-06 22:00:54 +0300

    Adding backend logic for registration processing

---
+++ src/main/java/com/resleteam/xlogbackendspring/controllers/RegistrationController.java
@@ -0,0 +1,34 @@
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RegistrationController {
+
+    @PostMapping("/api/register")
+    public ResponseEntity<String> register(@RequestBody RegistrationRequest request) {
+
+        if (!isValid(request)) {
+            return ResponseEntity.badRequest().body("Невірний формат даних");
+        }
+
+
+        boolean isRegistered = registerUser(request);
+
+        if (isRegistered) {
+            return ResponseEntity.ok("Користувач успішно зареєстрований");
+        } else {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Помилка при реєстрації");
+        }
+    }
+
+    private boolean isValid(RegistrationRequest request) {
+
+        return true;
+    }
+
+    private boolean registerUser(RegistrationRequest request) {
+
+        return true;
+    }
+}
