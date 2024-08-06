33421bcd26ee91358ff3b40e5f476c564f814155
Vlad Ĉereŝna <132610725+vladceresna@users.noreply.github.com>
2024-08-06 12:41:31 +0000

    Upd

--- src/main/java/com/resleteam/xlogbackendspring/services/PostsService.java
+++ src/main/java/com/resleteam/xlogbackendspring/services/PostsService.java
@@ -1,2 +1,12 @@
-package com.resleteam.xlogbackendspring.ser;
+package com.resleteam.xlogbackendspring.services;
 
+import java.util.UUID;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PostsService{
+
+  public String getPost(UUID id){
+    return "Hello World";
+  }
+}
